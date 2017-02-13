8bd5bba28d3fcbf6a43f9c6c35b98bec70072bc4
ichong <ichong@seattleu.edu>
2017-02-12 23:13:29 -0800

    gitbook add

---
+++ _book/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img id="header" src="./assets/todo.jpg">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 40px;
+  background-color: #E2E4F6;
+}
+
+#header {
+  width: 100%;
+}
+
+</style>
