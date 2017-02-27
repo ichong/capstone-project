c3969c1f6c8d9b75acd0bfdd96cf3fdb351de5af
ichong <ichong@seattleu.edu>
2017-02-26 22:02:37 -0800

    Revert "Fix layout and images"
    This reverts commit dfd9b115a12e9e3e090d00d97df6ad95e4f20d55.

--- src/components/main.vue
+++ src/components/main.vue
@@ -104,9 +104,15 @@ ul, li {
   border: 0;
 }
 
+body {
+  line-height: 1;
+  font-family: "Lato", sans-serif;
+  background-color: #E2E4F6;
+}
+
 .container {
   width: 70%;
-  margin: 90px auto 3em;
+  margin: 1em auto 3em;
   border: 1px solid #DEFFFC;
 }
 
@@ -189,6 +195,8 @@ button {
     width: 90%;
     max-width: 90%;
   }
-
+  button {
+    width: 80px;
+  }
 }
 </style>
